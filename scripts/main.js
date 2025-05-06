e51691c7b4a490ac24e4875a5acc5b4fa385ccce
Yousef fathy <yk972268@gmail.com>
2025-05-06 20:09:39 +0300

    web is more closer to be completed

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", () => {
-  const changeModeDiv = document.querySelector(".change-mode");
+  const changeModeDiv = document.querySelectorAll(".change-mode");
 
   // Apply correct mode when page fully loads
   let currentTheme = localStorage.getItem("theme");
@@ -11,7 +11,15 @@ document.addEventListener("DOMContentLoaded", () => {
 
   // Handle toggle on click
   if (changeModeDiv) {
-    changeModeDiv.addEventListener("click", () => {
+    changeModeDiv[0].addEventListener("click", () => {
+      let currentTheme = localStorage.getItem("theme");
+      if (currentTheme === "dark") {
+        window.changeToSun();
+      } else {
+        window.changeToMoon();
+      }
+    });
+    changeModeDiv[1].addEventListener("click", () => {
       let currentTheme = localStorage.getItem("theme");
       if (currentTheme === "dark") {
         window.changeToSun();
@@ -25,31 +33,37 @@ document.addEventListener("DOMContentLoaded", () => {
 // start dark mode fun
 window.changeToMoon = function () {
   document.body.classList.add("dark-mode");
-  let sun = document.querySelector(".sun-mode");
-  let moon = document.querySelector(".moon-mode");
-  let changeMode = document.querySelector(".change-mode");
-  let navbar = document.querySelector(".navbar");
+  let sun = document.querySelectorAll(".sun-mode");
+  let moon = document.querySelectorAll(".moon-mode");
+  let changeMode = document.querySelectorAll(".change-mode");
+  let navbar = document.querySelector(".nav-bar");
   let landing = document.querySelector(".landing");
   let services = document.querySelector(".services");
-  let projects = document.querySelector(".projects");
-  let footer = document.querySelector(".footer1");
+  let products = document.querySelector(".products");
   let contact = document.querySelector(".Contact");
   let textOnLanding = document.querySelector(".text-on-landing");
   let goUp = document.querySelector(".go-up");
 
-  if (sun) sun.style.transform = "scale(.84)";
-  if (sun) moon.style.transform = "scale(1)";
-  if (sun) sun.style.backgroundColor = "transparent";
-  if (sun) moon.style.backgroundColor = "white";
-  if (sun) sun.style.color = "var(--inactive-color-forSun)";
-  if (sun) moon.style.color = "var(--moon-color)";
-  if (changeMode) changeMode.style.backgroundColor = "var(--moon-color)";
+  if (sun) sun[0].style.transform = "scale(.84)";
+  if (sun) sun[1].style.transform = "scale(.84)";
+  if (sun) moon[0].style.transform = "scale(1)";
+  if (sun) moon[1].style.transform = "scale(1)";
+  if (sun) sun[0].style.backgroundColor = "transparent";
+  if (sun) sun[1].style.backgroundColor = "transparent";
+  if (sun) moon[0].style.backgroundColor = "white";
+  if (sun) moon[1].style.backgroundColor = "white";
+  if (sun) sun[0].style.color = "var(--inactive-color-forSun)";
+  if (sun) sun[1].style.color = "var(--inactive-color-forSun)";
+  if (sun) moon[0].style.color = "var(--moon-color)";
+  if (sun) moon[1].style.color = "var(--moon-color)";
+  if (changeMode) changeMode[0].style.backgroundColor = "var(--moon-color)";
+  if (changeMode) changeMode[1].style.backgroundColor = "var(--moon-color)";
 
   if (navbar) navbar.classList.add("dark-mode");
+  if (navbar) navbar.style.backgroundColor = "transparent";
   if (landing) landing.classList.add("dark-mode");
   if (services) services.classList.add("dark-mode");
-  if (projects) projects.classList.add("dark-mode");
-  if (footer) footer.classList.add("dark-mode");
+  if (products) products.classList.add("dark-mode");
   if (contact) contact.classList.add("dark-mode");
   if (textOnLanding) textOnLanding.classList.remove("text-black-50");
   if (goUp) goUp.classList.add("dark-mode");
@@ -59,31 +73,37 @@ window.changeToMoon = function () {
 
 window.changeToSun = function () {
   document.body.classList.remove("dark-mode");
-  let sun = document.querySelector(".sun-mode");
-  let moon = document.querySelector(".moon-mode");
-  let changeMode = document.querySelector(".change-mode");
-  let navbar = document.querySelector(".navbar");
+  let sun = document.querySelectorAll(".sun-mode");
+  let moon = document.querySelectorAll(".moon-mode");
+  let changeMode = document.querySelectorAll(".change-mode");
+  let navbar = document.querySelector(".nav-bar");
   let landing = document.querySelector(".landing");
   let services = document.querySelector(".services");
-  let projects = document.querySelector(".projects");
-  let footer = document.querySelector(".footer1");
-  let contact = document.querySelector(".Contact");
+  let products = document.querySelector(".products");
+  let contact = document.querySelector(".contact");
   let textOnLanding = document.querySelector(".text-on-landing");
   let goUp = document.querySelector(".go-up");
 
-  if (moon) moon.style.transform = "scale(.84)";
-  if (moon) sun.style.transform = "scale(1)";
-  if (moon) moon.style.backgroundColor = "transparent";
-  if (moon) sun.style.backgroundColor = "white";
-  if (moon) moon.style.color = "var(--inactive-color-forMoon)";
-  if (moon) sun.style.color = "var(--sun-color)";
-  if (changeMode) changeMode.style.backgroundColor = "var(--sun-color)";
+  if (moon) moon[0].style.transform = "scale(.84)";
+  if (moon) moon[1].style.transform = "scale(.84)";
+  if (moon) sun[0].style.transform = "scale(1)";
+  if (moon) sun[1].style.transform = "scale(1)";
+  if (moon) moon[0].style.backgroundColor = "transparent";
+  if (moon) moon[1].style.backgroundColor = "transparent";
+  if (moon) sun[0].style.backgroundColor = "white";
+  if (moon) sun[1].style.backgroundColor = "white";
+  if (moon) moon[0].style.color = "var(--inactive-color-forMoon)";
+  if (moon) moon[1].style.color = "var(--inactive-color-forMoon)";
+  if (moon) sun[0].style.color = "var(--sun-color)";
+  if (moon) sun[1].style.color = "var(--sun-color)";
+  if (changeMode) changeMode[0].style.backgroundColor = "var(--sun-color)";
+  if (changeMode) changeMode[1].style.backgroundColor = "var(--sun-color)";
 
   if (navbar) navbar.classList.remove("dark-mode");
+  if (navbar) navbar.style.backgroundColor = "white";
   if (landing) landing.classList.remove("dark-mode");
   if (services) services.classList.remove("dark-mode");
-  if (projects) projects.classList.remove("dark-mode");
-  if (footer) footer.classList.remove("dark-mode");
+  if (products) products.classList.remove("dark-mode");
   if (contact) contact.classList.remove("dark-mode");
   if (textOnLanding) textOnLanding.classList.add("text-black-50");
   if (goUp) goUp.classList.remove("dark-mode");
@@ -131,13 +151,21 @@ let navBar = document.querySelector(".nav-bar");
 let progress = document.querySelector(".progress-scroll");
 let adjusterMobile = document.querySelector(".landing .adjuster-mobile .landing-bee")
 let goUp = document.querySelector(".go-up");
+let mainCart = document.querySelector(".main-cart")
 
 window.onscroll = function () {
   if (window.scrollY >= 1) {
     navBar.style.borderBottom = "4px solid var(--gold-color)";
+    if (navBar.classList.contains("dark-mode")) {
+      navBar.style.backgroundColor = "black";
+    }
   } else {
     navBar.style.borderBottom = "4px solid transparent";
+    if (navBar.classList.contains("dark-mode")) {
+      navBar.style.backgroundColor = "transparent";
+    }
   }
+
   if (window.scrollY >= screen.height - 300) {
     adjusterMobile.style.animationPlayState = "running";
   }
@@ -146,7 +174,18 @@ window.onscroll = function () {
   } else {
     goUp.classList.remove("active");
   }
+
+  if (window.scrollY >= 300) {
+    mainCart.style.opacity = "1";
+  }
+
 };
 
 
+// pro-card funcitions
+
+let amountShow = document.querySelectorAll(".amount-show");
+let decAmount = document.querySelectorAll(".dec-amount");
+let incAmount = document.querySelectorAll(".inc-amount");
+let AddToCart = document.querySelectorAll(".add-to-cart");
 
